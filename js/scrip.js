ce73bd9bd2b84520891b8c9a47b841f501e397de
Adam Halada <adam.halada1@gmail.com>
2021-11-16 14:30:53 +0100

    kompletný re-dizajn  skill-set html scss js

--- js/scrip.js
+++ js/scrip.js
@@ -5,14 +5,14 @@
     overlay= $ ('<div/>', { id:'overlay'});
     overlay.appendTo ('body').hide();
 
-certification.find('a').on('click', function(event) 
-{	
+certification.find('a').on('click', function(event)
+{
     var href= 	$(this).attr('href'),
         image= 	$('<img>', {src: href, alt: 'poop' });
-        
+
         overlay.html(image)
              .fadeIn(300);
-             
+
         event.preventDefault();
 });
 
@@ -26,39 +26,16 @@ $(document).on('keyup', function(event){
 
 
 ////SKILL-SET////
-
-    var $bars = $( ".bar" ),
-        methods = {init: function() {  
-        methods.bindEvents();  
-    },
-    
-        bindEvents: function() {
-        $bars.each( function() {
-            
-        var $bar = $( this ),
-            $pct = $bar.find( ".pct" ),
-            data = $bar.data( "bar" );
-
-        setTimeout( function() {
-
-            $bar
-            .css( "background-color", data.color )
-            .animate({
-                "width": $pct.html()
-            },
-
-            data.speed || 3000, function() {
-                $pct.css({
-                "color": data.color,
-                "opacity": 1
-                });
-
-            });
-
-        }, data.delay || 0 );     
-
-        });
-    }};
-    methods.init(); 
+   var offsetTop = $('#skills').offset().top;
+    $(window).scroll(function() {
+  var height = $(window).height();
+  if($(window).scrollTop()+height > offsetTop) {
+    jQuery('.skillbar').each(function(){
+      jQuery(this).find('.skillbar-bar').animate({
+        width:jQuery(this).attr('data-percent')
+      },2000);
+    });
+  }
+  });
 
 })(jQuery);
